d22332eb3091afbfc1a732756636737c83cd2874
pierremaxencemazince <pierre-maxence.mazin@hotmail.fr>
2018-12-18 12:56:24 +0100

    Mise a jour v8

---
+++ Profilz/Profilz/Controllers/ErrorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Profilz.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int? id)
+        {
+            ViewData["errorCode"] = id.GetValueOrDefault(500);
+
+            ViewData["errorPage"] = Request.QueryString["aspxerrorpath"];
+
+            return View();
+        }
+
+        public ActionResult GetError(int? errorCode)
+        {
+            errorCode = errorCode.GetValueOrDefault(500);
+            string partial;
+
+            switch (errorCode)
+            {
+
+                case 404:
+                    partial = "Error404";
+                    break;
+
+                default:
+                    partial = "Error500";
+                    break;
+            }
+            return PartialView(partial);
+        }
+    }
+}
--- Profilz/Profilz/Controllers/HomeController.cs
+++ Profilz/Profilz/Controllers/HomeController.cs
@@ -26,14 +26,24 @@ namespace Profilz.Controllers
         public ActionResult User1(FormCollection collection)
         {
             ViewData["username"] = collection["username"];
-            
+
 
             return View("User2");
         }
 
-        public ActionResult userModel()
+        public ActionResult UserModel()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult UserModel(Models.User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("UserModelValidation", user);
+            }
+            return View(user);
         }
     }
 }
